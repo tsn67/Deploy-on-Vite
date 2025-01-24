07f8480b62baaf8f3163899c487c7f93f1f31f7e
tsn67 <tomsebastian67@gmail.com>
2025-01-24 20:35:13 +0530

    new package.json

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/', // Default to root. Adjust if deploying to a subdirectory.
   plugins: [react()],
-})
+});
